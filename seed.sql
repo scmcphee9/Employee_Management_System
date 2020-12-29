75ffda95a462b8677216235b5b84233141f20040
Shawn McPhee <scmcphee9@gmail.com>
2020-12-28 18:07:13 -0700

    added seed data to seed.sql instead of just schema

--- seed.sql
+++ seed.sql
@@ -24,6 +24,10 @@ role_id INT,
 manager_id INT
 );
 
+INSERT INTO department (deptname) VALUES ("Engineering"),("Finance"),("Legal"),("Sales");
+INSERT INTO roles (title, salary, department_id) VALUES("Sales Lead", 100000, 4),("Salesperson", 80000, 4),("Lead Engineer", 150000, 1),("Software Engineer", 100000, 1),("Account Manager", 160000, 2),("Accountant", 95000, 2),("Legal Team Lead", 200000, 3),("Lawyer", 190000, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES("Joe", "Schmoe", 1, 0),("John", "Doe", 2, 1),("Sneaky", "Pete", 3,0),("Jane", "Doe", 4, 1),("Mary", "Contrary", 5, 1),("Tom", "Hanks", 6, 1),("Billy", "Joel", 7, 1),("Dolly", "Parton", 8, 1);
+
 SELECT * FROM department;
 SELECT * FROM role;
 SELECT * FROM employee;
